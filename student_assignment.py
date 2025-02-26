2512ee290f59b657f8246c837460652703571b95
Fred Chen <fred_chen@asus.com>
2025-02-26 15:48:25 +0800

    feat - hw2

--- student_assignment.py
+++ student_assignment.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 import chromadb
 import traceback
 
@@ -28,8 +28,45 @@ def generate_hw01():
     return collection
 
 
-def generate_hw02(question, city, store_type, start_date, end_date):
-    pass
+def generate_hw02(
+    question: str,
+    city: list,
+    store_type: list,
+    start_date: datetime,
+    end_date: datetime,
+):
+    chroma_client = chromadb.PersistentClient(path=dbpath)
+    openai_ef = embedding_functions.OpenAIEmbeddingFunction(
+        api_key=gpt_emb_config["api_key"],
+        api_base=gpt_emb_config["api_base"],
+        api_type=gpt_emb_config["openai_type"],
+        api_version=gpt_emb_config["api_version"],
+        deployment_id=gpt_emb_config["deployment_name"],
+    )
+    collection = chroma_client.get_or_create_collection(
+        name="TRAVEL", metadata={"hnsw:space": "cosine"}, embedding_function=openai_ef
+    )
+
+    where_list = []
+    query_texts = [question] if question else ["search"]
+    if city:
+        where_list.append({"city": {"$in": city}})
+    if store_type:
+        where_list.append({"type": {"$in": store_type}})
+
+    if start_date:
+        where_list.append({"date": {"$gte": start_date.timestamp()}})
+    if end_date:
+        where_list.append({"date": {"$lte": end_date.timestamp()}})
+
+    query_results = collection.query(
+        query_texts=query_texts,
+        n_results=10,
+        where={"$and": where_list},
+        include=["metadatas"],
+    )
+
+    return [metadata.get("name") for metadata in query_results["metadatas"][0]]
 
 
 def generate_hw03(question, store_name, new_store_name, city, store_type):
